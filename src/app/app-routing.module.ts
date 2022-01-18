51461ec82f6a41399fa68dd7d1c4a220401a884c
christian-aguilar1 <christianaguilarm1@gmail.com>
2022-01-18 12:08:05 -0300

    add main modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: '/home', pathMatch: 'full', },
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+      },
+    ]
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule)
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
