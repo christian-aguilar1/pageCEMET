20828e950953a4021e0c80532c831a9109664ded
christian-aguilar1 <christianaguilarm1@gmail.com>
2022-03-02 11:38:29 -0300

    added create new component

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AdminComponent } from './components/admin/admin.component';
+import { CreateComponent } from './components/create/create.component';
 
 const routes: Routes = [
-  {path: '', component: AdminComponent}
+  {path: '', component: AdminComponent},
+  {path: 'crear', component: CreateComponent},
 ];
 
 @NgModule({
