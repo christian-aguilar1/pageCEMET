79e468bcd95cc9dfcd997f3e968a348618838bb0
christian-aguilar1 <christianaguilarm1@gmail.com>
2022-03-16 15:24:58 -0300

    created the component of a particular news

--- src/app/news/components/news/news.component.ts
+++ src/app/news/components/news/news.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Params } from '@angular/router';
+
+import { FirestoreService } from 'src/app/core/services/db/firestore/firestore.service';
 
 @Component({
   selector: 'app-news',
@@ -8,11 +12,34 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class NewsComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  idDoc: string = "";
+  public news = {} as  any;
+  image!: string;
+
+  public months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+
+  constructor(private firestoreService: FirestoreService, private storage: AngularFireStorage, private title: Title,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
-      const id = params['id'];
+      this.idDoc = params['id'];
+    })
+
+    this.firestoreService.getCollection('news', this.idDoc).subscribe((doc) => {
+      this.news = [];
+      let newsDB = doc.data() as any;
+      let a = newsDB.date.toDate()
+      let date = a.getDate() + " de " + this.months[a.getMonth()] + ", " + a.getFullYear();
+      newsDB.date = date;
+      this.news = newsDB;
+      if (this.news.image !== "") {
+        const fileRef = this.storage.ref(this.news.image);
+        const imageRef = fileRef.getDownloadURL();
+        imageRef.subscribe(url => {
+          this.image = url;
+        })
+      }
     })
   }
 
